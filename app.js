69570b85de0f2ffbeeca78327a301d7d4949e633
RoshiniRaja <47032474+RoshiniRaja@users.noreply.github.com>
2019-01-25 19:36:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -93,3 +93,26 @@ async function getData() {
 		dispNode(result,"flex");
     }
 }
+
+window.addEventListener('load', async e => {
+    console.log(navigator.onLine);
+    if ('serviceWorker' in navigator) {
+        try {
+            navigator.serviceWorker.register('serviceworker.js');
+            console.log('SW registered');
+        } catch (error) {
+            console.log('SW failed');
+
+        }
+    }
+   /* if(navigator.onLine){
+        navigator.serviceWorker.controller.postMessage("online");
+    }
+    else
+    {
+        displayNotification('No Internet','Please connent to a network to search a new word');
+        navigator.serviceWorker.controller.postMessage("offline");
+    }
+    // await getData();*/
+});
+
